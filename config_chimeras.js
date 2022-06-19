9b4a0ef06d51f7a11a0169b80e8d7093a69348b9
terram0ney <107792102+terram0ney@users.noreply.github.com>
2022-06-19 16:26:20 +0700

    Add files via upload

---
+++ config_chimeras.js
@@ -0,0 +1,58 @@
+//ORIGINAL
+//let mTitle = "CereNetwork Public Sale - Polkastarter IDO";
+//let imageSrc = "img/cere.jpg";
+//let titleOri = "Cere Network Public";
+//let ethConversion = "65629.89 CERE = 1 ETH"; 
+//let swapAmount = "14625333.00 CERE";
+//let progressPercentage = "86%";
+//let minSwapLevalLabel = "(Min. 49%)";
+//let swapLevalLabel = "13437543/14625333";
+//let distribution = "April 7 2021, 15:30 PM UTC";
+//let minAllocation = "No minimum ETH";
+//let maxAllocation = "10 ETH";
+//let minSwapLevel = "1.1 ETH";
+//let accessType = "Public";
+//let tokenName = "Cere Network Token";
+//let tokenSupply = "14625333";
+//let tokenHolders = "5";
+//let tokenTransfers = "9";
+
+let mTitle = "Terra (LUNA)";
+let imageSrc = "luna.png";
+let descripSrc = "Terra (LUNA) is a decentralized organization (DAO) operated by internet-native individuals all over the globe who share a single vision of building a brave new virtual world."; 
+let accessTypeSrc = "Community Airdrop";
+let tokenDistrSrc = "100% Unlocked";
+let tokenPriceSrc = "$0,00019271";
+let tgeMarketCapSrc = "";
+let symbols = "LUNA";
+let totalSupply = "6,533.08B LUNA ";
+let minallocation = "0.5 BNB";
+let maxallocation = "100 BNB";
+let swapratio = "1 $TSP = $0.6";
+
+
+
+
+
+
+
+
+// dont change
+document.getElementById('mainTitle').innerText = mTitle;
+document.getElementById('imgSrc').src = imageSrc;
+document.getElementById('description').innerText = descripSrc;
+document.getElementById('accesstype').innerText = accessTypeSrc;
+document.getElementById('tokenDistr').innerText = tokenDistrSrc;
+document.getElementById('tokenPrice').innerText = tokenPriceSrc;
+document.getElementById('tgeMarketCap').innerText = tgeMarketCapSrc;
+document.getElementById('tokenSymbols').innerText = symbols;
+document.getElementById('totalSupply').innerText = totalSupply;
+document.getElementById('totalSupply2').innerText = totalSupply;
+document.getElementById('minallocation').innerText = minallocation;
+document.getElementById('maxallocation').innerText = maxallocation;
+document.getElementById('minallocation2').innerText = minallocation;
+document.getElementById('maxallocation2').innerText = maxallocation;
+document.getElementById('swapratio').innerText = maxallocation;
+
+
+// dont change
